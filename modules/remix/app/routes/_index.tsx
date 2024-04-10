38ae798743f3e82a935a39e23b40199eae4e08cc
kiliman <kiliman@gmail.com>
2024-04-10 12:08:43 -0400

    Use Remix `defer` for test parity

--- modules/remix/app/routes/_index.tsx
+++ modules/remix/app/routes/_index.tsx
@@ -1,26 +1,35 @@
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { defer } from "@remix-run/node";
+import { Await, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
 import { testData } from "testdata";
 
 export const loader = async () => {
-  return json(await testData());
+  return defer({ testData: testData() });
 };
 
 export default function App() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <table>
-      <tbody>
-        {data.map((entry) => (
-          <Entry key={entry.id} entry={entry} />
-        ))}
-      </tbody>
-    </table>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Await resolve={data.testData}>
+        {(data) => (
+          <table>
+            <tbody>
+              {data.map((entry) => (
+                <Entry key={entry.id} entry={entry} />
+              ))}
+            </tbody>
+          </table>
+        )}
+      </Await>
+    </Suspense>
   );
 }
 
-function Entry(props: { entry: { id: string; name: string; asyncData?: () => Promise<string> } }) {
+function Entry(props: {
+  entry: { id: string; name: string; asyncData?: () => Promise<string> };
+}) {
   return (
     <tr>
       <td>{props.entry.id}</td>
