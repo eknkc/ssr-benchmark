6ed0a7f9e25095933b27aa7e6a805b867e406977
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-11 21:56:19 +0100

    fix react `content-type`
    `text/plain` -> `text/html`

--- modules/react/src/entry-server.tsx
+++ modules/react/src/entry-server.tsx
@@ -6,7 +6,7 @@ import { IncomingMessage, ServerResponse } from "http";
 export async function buildHandler() {
   return async function handler(_: IncomingMessage, res: ServerResponse) {
     const stream = ReactDOMServer.renderToPipeableStream(<App />);
-    res.setHeader("content-type", "text/plain");
+    res.setHeader("content-type", "text/html");
     stream.pipe(res);
   };
 }
